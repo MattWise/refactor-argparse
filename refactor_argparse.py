5c27b328ad1507820d0b7be50ec71d3e2eac73f3
Matthew Wise <ttam.esiw@gmail.com>
2016-08-08 21:10:52 -0400

    Completed build_signature and build_docstring.

--- refactor_argparse.py
+++ refactor_argparse.py
@@ -1,11 +1,19 @@
-import argparse,re
+import argparse,re,os
 
 argument_re=re.compile(r".*add_argument")
 
-def extract_args(file_path):
+line_sep="\n"
+indent="\t"
+
+def load_file(file_path):
+    with open(os.path.abspath(file_path),"r") as f:
+        text=f.read()
+    return text
+
+def extract_args(text):
 
     def get_arg(text,start):
-        assert(text[start]=="(")
+        assert text[start]=="(","First Char: {}".format(text[start])
         i=start
         net=1
         while net!=0:
@@ -24,18 +32,78 @@ def extract_args(file_path):
     with open(file_path,"r") as f:
         text=f.read()
         for m in argument_re.finditer(text):
-            start=m.span()[-1]+1
+            start=m.span()[-1]
             args.append(get_arg(text,start))
 
     return args
 
-def extract_dcts(file_path):
+def extract_dcts(text):
 
-    args=extract_args(file_path)
+    args=extract_args(text)
 
     parser=argparse.ArgumentParser()
     for arg in args:
-        eval("parser.add_argument({})".format(arg))
+        eval("parser.add_argument{}".format(arg))
+
+    help_dct={}
+    default_dct={}
+    type_dct={}
+
+    dcts=help_dct,default_dct,type_dct
 
     for action in parser._actions:
-        print action
+        help_dct[action.dest]=action.help
+        default_dct[action.dest]=action.default if action.default!= '==SUPPRESS==' else None
+        if type(action)==argparse._StoreTrueAction or type(action) == argparse._StoreFalseAction:
+            type_dct[action.dest]=bool
+        else:
+            type_dct[action.dest]=action.type
+
+    for dct in dcts:
+        if 'help' in dct: del dct['help']
+
+    return dcts
+
+def build_arg_list(default_dct):
+    arg_list = []
+    for arg, default in default_dct.items():
+        if default is not None:
+            arg_list.append("{}={}".format(arg,default))
+        else:
+            arg_list.append(arg)
+    arg_list.sort(key=lambda arg_token: not "=" in arg_token)
+    return arg_list
+
+def build_signature(arg_list):
+    args=",".join(arg_list)
+    return "def main("+args+")"
+
+def build_docstring(help_dct,type_dct):
+    sep=line_sep+indent
+    params=[]
+    types=[]
+
+    for arg in help_dct.keys():
+        h,t=help_dct[arg],type_dct[arg]
+        params.append(':param {}: {}'.format(arg,h))
+        types.append(':type {}: {} [insert type description]'.format(arg,t))
+
+    params=sep.join(params)
+    types= sep.join(types)
+
+    return sep.join(['"""',params,types,'"""'])
+
+
+
+def build_main(text):
+    dcts= help_dct, default_dct, type_dct=extract_dcts(text)
+
+
+
+def main(file_path):
+    text=load_file(file_path)
+    main=build_main(text)
+
+if __name__ == '__main__':
+    help_dct, default_dct, type_dct=extract_dcts("./test_input.py")
+    print help_dct,default_dct,type_dct
--- test_input.py
+++ test_input.py
@@ -1,11 +1,23 @@
 import argparse
 
 def main():
-    print args.foo
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pairs', type=str, default="./pairs.txt",
+                        help='Location of the LFW pairs file from http://vis-www.cs.umass.edu/lfw/pairs.txt')
+    parser.add_argument('--lfw-align', type=str, default="./lfw-align",
+                        help='The directory of lfw-align, which contains the aligned lfw images')
+    parser.add_argument('--suffix', type=str, default="png",
+                        help='The type of image')
+    parser.add_argument('--size', type=int, default=128,
+                        help='the image size of lfw aligned image, only support squre size')
+    parser.add_argument('--model-prefix', default='../model/lightened_cnn/lightened_cnn',
+                        help='The trained model to get feature')
+    parser.add_argument('--epoch', type=int, default=165,
+                        help='The epoch number of model')
+    parser.add_argument('--predict-file', type=str, default='./predict.txt',
+                        help='The file which contains similarity distance of every pair image given in pairs.txt')
+    parser.add_argument('bar', action="store_true", help='bar help')
+    args = parser.parse_args()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--foo', default="hello", help='foo help')
-    parser.add_argument('bar', action="store_true", help='bar help')
-    args=parser.parse_args()
     main()
